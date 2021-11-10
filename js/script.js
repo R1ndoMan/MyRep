398afc1d458a71141055a1588c5b9faa04e12440
Ermek <se@1ncome.kz>
2021-11-10 17:59:22 +0600

    my first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"Use strict";
+let a = 2;
+console.log(a);
+
+let url = "toys";
+let urlFuck = "fuck";
+    url = urlFuck;
+    urlFuck = `плохое слово ${url}`;
+
+console.log(`тут будет текст ${urlFuck}`);
+
+let names = "Ermek";
+    profession = "Айтишник"
+    job = "Payda";
+
+    console.log(`Меня зовут ${names}, я работаю ${profession}ом в компании ${job}`);
+
+
+
